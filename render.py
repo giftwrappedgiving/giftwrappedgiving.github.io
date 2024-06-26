dddfdcab57cc4c1d8d3a4d1b7a4d683241734281
colmjude <colmjude@gmail.com>
2024-06-26 09:41:34 +0300

    Set up a styleguide page for testing stuff

--- render.py
+++ render.py
@@ -1,17 +1,15 @@
-from bin.jinja_setup import setup_jinja, render
-
+from bin.jinja_setup import render, setup_jinja
 
 env = setup_jinja()
 
 
 def generate_pages():
     home_template = env.get_template("home.html")
-    # render the full backlog
-    render(
-        "./docs/index.html",
-        home_template,
-        name="GWG"
-    )
+    styleguide_template = env.get_template("styleguide.html")
+    # render the homepage
+    render("./docs/index.html", home_template, name="GWG")
+    # render the styleguide
+    render("./docs/styleguide.html", styleguide_template)
 
 
 if __name__ == "__main__":
