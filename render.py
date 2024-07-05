d9fe83793bd1a87a9f6fa67c1d048d15c19fee36
colmjude <colmjude@gmail.com>
2024-07-05 23:09:48 +0300

    set up how we'll do list pages

--- render.py
+++ render.py
@@ -5,11 +5,14 @@ env = setup_jinja()
 
 def generate_pages():
     home_template = env.get_template("home.html")
+    guides_template = env.get_template("guides.html")
     styleguide_template = env.get_template("styleguide.html")
     # render the homepage
     render("./docs/index.html", home_template, name="GWG")
     # render the styleguide
     render("./docs/styleguide.html", styleguide_template)
+    # render the styleguide
+    render("./docs/guides.html", guides_template)
 
 
 if __name__ == "__main__":
