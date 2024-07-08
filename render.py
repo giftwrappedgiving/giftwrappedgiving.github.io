e953fd5c84784a5a5cf70771c97a4bfdeafba197
colmjude <colmjude@gmail.com>
2024-07-08 21:35:23 +0300

    set up and link to example guide page

--- render.py
+++ render.py
@@ -6,13 +6,16 @@ env = setup_jinja()
 def generate_pages():
     home_template = env.get_template("home.html")
     guides_template = env.get_template("guides.html")
+    guide_template = env.get_template("guide.html")
     styleguide_template = env.get_template("styleguide.html")
     # render the homepage
     render("./docs/index.html", home_template, name="GWG")
     # render the styleguide
     render("./docs/styleguide.html", styleguide_template)
-    # render the styleguide
+    # render the list of guides
     render("./docs/guides.html", guides_template)
+    # render a guide page
+    render("./docs/guide.html", guide_template)
 
 
 if __name__ == "__main__":
