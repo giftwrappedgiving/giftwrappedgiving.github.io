6ecbcde1721f1875bc329c7f8c8bf5d5b4d7d8d8
colmjude <colmjude@gmail.com>
2024-07-21 10:23:57 +0300

    Set up about page

--- render.py
+++ render.py
@@ -10,6 +10,7 @@ def generate_pages():
     home_template = env.get_template("home.html")
     guides_template = env.get_template("guides.html")
     guide_template = env.get_template("guide.html")
+    about_template = env.get_template("about.html")
     styleguide_template = env.get_template("styleguide.html")
     # render the homepage
     render("./docs/index.html", home_template, name="GWG")
@@ -19,6 +20,8 @@ def generate_pages():
     render("./docs/guides.html", guides_template)
     # render a guide page
     render("./docs/guide.html", guide_template)
+    # render a about page
+    render("./docs/about.html", about_template)
 
 
 if __name__ == "__main__":
